393a88b31c55ead8c9ebf4ece9a2d23e145ae2d2
arthurfurtunato <arthurfurtunato@gmail.com>
2022-09-19 21:49:13 -0300

    feat: add controller

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
+
+@Controller('courses')
+export class CoursesController {
+  constructor(private readonly courseService: CoursesService) {}
+
+  @Get()
+  findAll() {
+    return this.courseService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.courseService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body) {
+    return this.courseService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.courseService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.courseService.remove(id);
+  }
+}
